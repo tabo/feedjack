416ee70e68786352ea6466a8fe9d87010e907d54
Gustavo Picon <tabo@tabo.pe>
2007-07-09 06:07:25 +0000

    Changnig temporary version
    --HG--
    extra : convert_revision : svn%3A25b5c7c2-9819-0410-812b-05e975a4e153/trunk%4041

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from finddata import find_package_data
 
 setup(
     name = 'Feedjack',
-    version = '0.9.10',
+    version = '0.9.11a',
     url = 'http://www.feedjack.org/',
     author = 'Gustavo Picón',
     author_email = 'gpicon@aureal.com.pe',
